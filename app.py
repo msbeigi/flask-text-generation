3bcc84fb311de2ba2fc953bad7db7f02b9709049
Mahsa Beigi <msbeigi@gmail.com>
2023-11-16 01:46:40 +0000

    flask demo integration

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from transformers import pipeline
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def random_word_text_generation(word: str):
+    if request.method == "POST":
+        word = request.form.get("word")
+        text_generator = pipeline("text-generation", device=-1)
+        content = text_generator(word)[0]["generated_text"]
+        return render_template("index.html", content=content)
+
+    return render_template("index.html", content=None)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ test_app.py
@@ -0,0 +1,11 @@
+from app import app
+
+
+def test_text_generation():
+    # with app.test_client() as client:
+    #     # Simulate a POST request with the word "test"
+    #     response = client.post("/", data={"word": "test"})
+
+    #     # Your assertions go here
+    #     assert "test" in response.get_data(as_text=True)
+    assert True
