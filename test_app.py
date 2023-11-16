fcafb98378a1d3f5dbdd6f0669547678cf5a1585
Mahsa Beigi <msbeigi@gmail.com>
2023-11-16 02:27:54 +0000

    Edited test to echo the result

--- test_app.py
+++ test_app.py
@@ -5,7 +5,8 @@ def test_text_generation():
     with app.test_client() as client:
         # Simulate a POST request with the word "test"
         response = client.post("/", data={"word": "apple"})
-
+        content_generated=response.get_data(as_text=True)
+        print(content_generated)
         # Your assertions go here
         assert "apple" in response.get_data(as_text=True)
     # assert True
